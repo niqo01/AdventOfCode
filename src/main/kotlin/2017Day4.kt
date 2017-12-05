b9a84907f2a3581d51bd7cabcfbe35ab5fc9cf83
Nicolas Milliard <niqo01@gmail.com>
2017-12-04 21:57:36 -0800

    Day4 part 2

--- src/main/kotlin/2017Day4.kt
+++ src/main/kotlin/2017Day4.kt
@@ -1,17 +1,44 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    val uri = Unit.javaClass.getResource("/input.txt").toURI()
-    var validCount = File(uri)
+    val input = File(Unit.javaClass.getResource("/input.txt").toURI())
+    part1(input)
+    part2(input)
+}
+
+fun part1(input: File) {
+    var validCount = input
             .readLines()
             .asSequence()
             .count { isValidPass(it) }
-    println("Result: $validCount")
+    println("Part 1 Result: $validCount")
 }
 
 fun isValidPass(line: String): Boolean {
     val original = line.split("\\s+".toRegex())
     val set = original.toHashSet()
+    return original.size == set.size
+}
+
+fun part2(input: File) {
+    var validCount = input
+            .readLines()
+            .asSequence()
+            .count { isValidPassPart2(it) }
+    println("Part 2 Result: $validCount")
+}
+
+fun isValidPassPart2(line: String): Boolean {
+    val original = line.split("\\s+".toRegex())
+
+    val set = original.asSequence()
+            .map { s ->
+                s.asSequence()
+                        .sorted()
+                        .map { s -> s.toString() }
+                        .reduce { acc, s -> acc + s }
+            }
+            .toHashSet()
     return original.size == set.size
 }
 
